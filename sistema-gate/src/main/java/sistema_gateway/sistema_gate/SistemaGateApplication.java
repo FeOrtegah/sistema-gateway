77110d2b3e48b07e116f0cc11ae3bc4ea2207596
Antonio Enrique Cornejo Morales <ant.cornejom@duocuc.cl>
2026-05-04 23:25:54 -0400

    first commit

---
+++ sistema-gate/src/main/java/sistema_gateway/sistema_gate/SistemaGateApplication.java
@@ -0,0 +1,13 @@
+package sistema_gateway.sistema_gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGateApplication.class, args);
+	}
+
+}
---
+++ sistema-gate/src/test/java/sistema_gateway/sistema_gate/SistemaGateApplicationTests.java
@@ -0,0 +1,13 @@
+package sistema_gateway.sistema_gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
