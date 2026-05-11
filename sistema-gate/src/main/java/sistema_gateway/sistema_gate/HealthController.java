8156ae177a0d9b3cbaf2392d71673bcc454b1186
TheAntonio <ant.cornejom@duocuc.cl>
2026-05-11 16:00:35 -0400

    Update HealthController.java

--- sistema-gate/src/main/java/sistema_gateway/sistema_gate/HealthController.java
+++ sistema-gate/src/main/java/sistema_gateway/sistema_gate/HealthController.java
@@ -1,18 +1,13 @@
 package sistema_gateway.sistema_gate;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.mvc.config.GatewayMvcProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-    @Autowired
-    private GatewayMvcProperties properties;
-
     @GetMapping("/")
     public String health() {
-        return "Gateway funcionando. Rutas: " + properties.getRoutes().size();
+        return "Gateway funcionando";
     }
 }
