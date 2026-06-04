b864dcd1a2c0c15f07c622f7114221432dd82628
FeOrtegah <fe.ortegah@duocuc.cl>
2026-06-03 23:53:25 -0400

    actualizacion cors para usar allowedoriginpatterns

--- sistema-gate/src/main/java/sistema_gateway/sistema_gate/CorsConfig.java
+++ sistema-gate/src/main/java/sistema_gateway/sistema_gate/CorsConfig.java
@@ -6,16 +6,18 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPatterns(List.of("https://react-frontend-senb.onrender.com"));
+        config.setAllowedOriginPatterns(List.of("https://react-frontend-senb.onrender.com"));
+        config.setAllowCredentials(true);
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
